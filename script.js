fab1353e897e4d4aa8c51aa2c953bda6509c3f68
Tim Gabrikowski <tim@gabrikowski.de>
2023-04-27 18:40:56 +0200

    add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+function getIOTD() {
+	var url = "https://api.nasa.gov/planetary/apod?api_key=" + nasa_api_key;
+	var request = new XMLHttpRequest();
+	request.open("GET", url);
+	request.onreadystatechange = function () {
+		if (request.readyState === XMLHttpRequest.DONE) {
+			var status = request.status;
+			if (status === 0 || (status >= 200 && status < 400)) {
+				var response = JSON.parse(request.responseText);
+				updatePage(response);
+			}
+		}
+	};
+	request.send();
+}
+
+let image = document.getElementById("image");
+let iframe = document.getElementById("iframe");
+
+let date = document.getElementById("date");
+let title = document.getElementById("title");
+let description = document.getElementById("description");
+let copyright = document.getElementById("copy");
+
+function updatePage(response) {
+	if (response.media_type === "image") {
+		image.src = response.hdurl || response.url;
+		image.style.display = "block";
+		iframe.style.display = "none";
+	} else {
+		image.style.display = "none";
+		iframe.style.display = "iframe";
+	}
+
+	copyright.innerText = response.copyright || "Public Domain Image";
+
+	date.innerText = response.date || "";
+	title.innerText = response.title || "";
+	description.innerText = response.explanation || "";
+}
+getIOTD();
